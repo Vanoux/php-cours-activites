f3437f06f8e788e8ee7965d67f1848f72a06fb1c
Vanoux <vanessa.roux891@orange.fr>
2018-07-30 16:14:29 +0200

    exo boucles

--- boucles/for.php
+++ boucles/for.php
@@ -96,12 +96,14 @@ $vehiculeConcession = array(
 );
 echo '7.Les voitures dans la concession :';
 foreach ($vehiculeConcession as $gamme => $vehicule) {
-    echo '<br>'.$gamme;
+    echo '<br>'.$gamme . ' : ' . '<br>';
     foreach ($vehicule as $propriete => $valeur) {
         echo $propriete.' : '.$valeur.'<br>';
     }
 }
 echo '<br><br>';
+
+
 
 /*###############################################*/
 /*################# ACTIVITÉS ###################*/
@@ -110,21 +112,33 @@ echo '<br><br>';
 //----------------------------------------
 //Créer un tableau pour les mois de l'année et affiché tous les mois de Janvier à Décembre
 //modifier et/ou remplacer les éléments ci-dessous
-echo "8.Les mois depuis le debut de l'annee : <br>";
-$mois = [];
-for ($i = 0; $i < 0; $i) {
-    echo '';
+
+$mois = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+echo "8.Les mois depuis le debut de l'annee : <br><br>";
+// for ($i = 0; $i < 12; $i++) {
+//     echo $mois[$i] . '<br>';
+//} 
+
+//fonctionne aussi avec un foreach :
+foreach ($mois as $mois){
+  echo $mois . '<br>';
 }
 echo '<br><br>';
 
+
 //Afficher les mois de la fin de l'année jusqu'au début de l'année
 //modifier et/ou remplacer les éléments ci-dessous
+
+$mois = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 echo "9.Les mois depuis la fin de l'annee : <br>";
-for ($i = 0; $i < 0; $i) {
-    echo '';
+for ($i = 12; $i > 0; $i--) {
+    echo $mois[$i] . '<br>';
 }
 echo '<br><br>';
+
 //----------------------------------------
+
+
 //Afficher le nom et prénoms des élèves de ce collège
 $college = array(
   'Sixieme' => array(
@@ -149,16 +163,59 @@ $college = array(
 
 echo '10.Les eleves du college : <br>';
 //ajoutez votre code ici
+//syntax : foreach($tableau as $clef => $valeur){}
+
+foreach ($college as $key => $value){
+echo '<br>' . $key. ': ' . '<br><br>';
+    foreach ($value as $key2 => $valeur){
+      echo $valeur["Prenom"] . ', ' . $valeur["Nom"] . '<br>';
+    }
+}
 echo '<br><br>';
 
 //----------------------------------------
 //Afficher le nom et prénoms des élèves de ce collège
 //reprenez le tableau ci-dessus, ajoutez des éléves pour la classe de troisième et réaffichez tout
+$college = array(
+  'Sixieme' => array(
+    array('Nom' => 'Payet', 'Prenom' => 'Mickael'),
+    array('Nom' => 'Hoareau', 'Prenom' => 'Christine'),
+    array('Nom' => 'Maillot', 'Prenom' => 'Laure'),
+  ),
+  'Cinquieme' => array(
+    array('Nom' => 'Bourdon', 'Prenom' => 'Didier'),
+    array('Nom' => 'Legitimus', 'Prenom' => 'Pascal'),
+    array('Nom' => 'Campan', 'Prenom' => 'Bernard'),
+    array('Nom' => 'Fois', 'Prenom' => 'Marina'),
+    array('Nom' => 'Floresti', 'Prenom' => 'Florence'),
+  ),
+  'Quatrieme' => array(
+    array('Nom' => 'Willis', 'Prenom' => 'Bruce'),
+    array('Nom' => 'Lawrence', 'Prenom' => 'Laurence'),
+    array('Nom' => 'Johannson', 'Prenom' => 'Scarlett'),
+    array('Nom' => 'Jackson', 'Prenom' => 'Samuel'),
+  ),
+  'Troisième' => array(
+    array('Nom' => 'Barra', 'Prenom' => 'Marius'),
+    array('Nom' => 'Pépone', 'Prenom' => 'Luciano'),
+    array('Nom' => 'Marshall', 'Prenom' => 'Paula'),
+    array('Nom' => 'Paris', 'Prenom' => 'Suze'),
+  ),
+);
 echo '11.Les eleves du college (avec les nouveaux arrivants): <br>';
 //ajoutez votre code ici
+foreach ($college as $key => $value){
+  echo "<br>" . $key . " : " . "<br><br>";
+  foreach ($value as $key2 => $value2){
+    echo $value2['Nom'] . " , " . $value2['Prenom'] . "<br>";
+  } 
+}
 echo '<br><br>';
 
+
 //----------------------------------------
+
+
 //Afficher toutes les informations de la vidéothèque
 $videotheque = array(
   array(
@@ -195,8 +252,30 @@ $videotheque = array(
   ),
 );
 
-echo '12.Mes films : <br>';
+echo '12.Mes films : <br><br>';
 //ajoutez votre code ici
+
+//Cette facon marche mais bof :
+//   foreach ($videotheque as $key => $value){
+//    foreach ($value as $key2 => $value2){
+//      echo  $key2 . " : " . $value2 . "\n" . "<br>";
+//      foreach ($value2 as $key3 => $value3){
+//       echo  $value3 . "<br><br>";
+//      }
+//    }
+// echo "<br>";
+//  }
+
+ foreach ($videotheque as $key => $value){
+  echo "Nom : " . $value['nom']. "<br>" ."Date :" . $value['date'] . "<br>" . "Réalisateur : " . $value['realisateur'] . "<br>";
+ foreach ($value as $key2 => $value2){
+   foreach ($value2 as $key3 => $value3){
+    echo  "Acteur : " . $value3 . "\n" . "<br>";
+   }
+ }
+ echo "<br>";
+}
+
 echo '<br><br>';
 
 //----------------------------------------
@@ -205,6 +284,58 @@ echo '<br><br>';
 //d'informations (nom, date, realisateur, acteurs) et en plus de ces informations
 //rajoutez un synopsis
 
+$videotheque = array(
+  array(
+    'nom' => 'Independance day',
+    'date' => 1996,
+    'realisateur' => 'Roland Emmerich',
+    'acteurs' => array(
+      'Will Smith', 'Bill Pullman', 'Jeff Goldblum', 'Mary McDonnell',
+    ),
+  ),
+  array(
+    'nom' => 'Bienvenue a Gattaca',
+    'date' => 1998,
+    'realisateur' => 'Andrew Niccol',
+    'acteurs' => array(
+      'Ethan Hawke', 'Uma Thurman', 'Jude Law',
+    ),
+  ),
+  array(
+    'nom' => 'Forrest Gump',
+    'date' => 1994,
+    'realisateur' => 'Robert Zemeckis',
+    'acteurs' => array(
+      	'Tom Hanks', 'Gary Sinise',
+    ),
+  ),
+  array(
+    'nom' => '12 hommes en colere',
+    'date' => 1957,
+    'realisateur' => 'Sidney Lumet',
+    'acteurs' => array(
+      	'Henry Fonda','Martin Balsam','John Fiedler','Lee J. Cobb','E.G. Marshall',
+    ),
+  ),
+  array(
+    'nom' => 'Kill Bill',
+    'date' => 2003,
+    'realisateur' => 'Tarantino',
+    'acteurs' => array(
+      'Uma Thurman', 'Lucy Liu', 'Sonny Chiba',
+    ),
+    'synopsis' => 'Au cours d\'une cérémonie de mariage en plein désert, un commando fait irruption dans la chapelle et tire sur les convives.<br> Laissée pour morte, la Mariée enceinte retrouve ses esprits après un coma de quatre ans.',
+  )
+);
 echo '13.Mes films : <br>';
 //ajoutez votre code ici
+foreach ($videotheque as $key => $value){
+  echo "Nom : " .  $value['nom'] . "<br>" . "Date : " . $value['date'] . "<br>" . "Réalisateur : " . $value['realisateur'] . "<br>" . "Synopsis : " .$value['synopsis'] . "<br>";
+  foreach ($value as $key2 => $value2){
+    foreach ($value2 as $key3 => $value3){
+      echo "Acteurs : " . $value3 . "\n" . "<br>";
+    }
+  }
+  echo '<br>';
+} 
 echo '<br><br>';
